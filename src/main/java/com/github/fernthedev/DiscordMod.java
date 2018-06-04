010d101bbd694cf3ce25fc2a586e92ace150c32a
Fern <15272073+Fernthedev@users.noreply.github.com>
2018-06-04 17:16:48 -0400

    Updated version number in mod class.

--- src/main/java/com/github/fernthedev/DiscordMod.java
+++ src/main/java/com/github/fernthedev/DiscordMod.java
@@ -25,7 +25,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class DiscordMod {
     public static final String MODID = "discordmod";
     public static final String NAME = "Discord";
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.2.1";
 
     private RPC rpc;
     public static String client;
