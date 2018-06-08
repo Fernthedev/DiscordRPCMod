256644e07ffe4896c5c28e4b5cacab8cdd7bf510
Fern <15272073+Fernthedev@users.noreply.github.com>
2018-06-08 15:16:12 -0400

    Changed version number

--- src/main/java/com/github/fernthedev/DiscordMod.java
+++ src/main/java/com/github/fernthedev/DiscordMod.java
@@ -29,7 +29,7 @@ import java.io.File;
 public class DiscordMod {
     public static final String MODID = "discord";
     public static final String NAME = "Discord";
-    public static final String VERSION = "1.3";
+    public static final String VERSION = "1.3.2";
 
     public static String client;
     public static EntityPlayer player;
